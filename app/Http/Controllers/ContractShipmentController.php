67c1365a236d38476d60bb607b5ba54e7e77647e
Egemen KIRKAPLAN <atamyazilim@gmail.com>
2023-01-11 17:31:57 +0300

    Bu ne lo

--- app/Http/Controllers/ContractShipmentController.php
+++ app/Http/Controllers/ContractShipmentController.php
@@ -125,15 +125,14 @@ class ContractShipmentController extends Controller
 
     private function squadCalc(Squad $squad, string $date, $arrivalTonnage)
     {
-        $unitPrice = $squad->unitPrices()->get()
-            ->where('start_date', '>=', $date)
-            ->where('end_date', '<=', $date)
+        $unitPrice = $squad->unitPrices()
+            ->where('start_date', '<=', $date)
+            ->where('end_date', '>=', $date)
             ->first();
-        dd($unitPrice);
         if (is_null($unitPrice)) {
             return 0;
         } else {
-            return $unitPrice->price * (int) $arrivalTonnage;
+            return $unitPrice->price * (int)$arrivalTonnage;
         }
     }
 }
